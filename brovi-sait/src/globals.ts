aa8e492f101e82133d9bdc740aa814e01777c862
Senya <fnafbodnaf@gmail.com>
2026-09-18 17:34:08 +0300

    глобальные переменные и тд

---
+++ brovi-sait/src/globals.ts
@@ -0,0 +1,6 @@
+export const brand_name = "Brovi";
+export const work_time = "Пн-Вс 10:00-20:00";
+export const phone_number = "8-999-999-99-99";
+export const phone_number_raw = "89999999999";
+export const branch_address = "г.Чебоксары ул.БезымяннаяВысота дом 3, 1 этаж";
+export const email = "ivanov_pupkin@gmail.ru";
